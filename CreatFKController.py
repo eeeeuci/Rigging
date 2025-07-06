4e461409712b0efd2fa6633c9b45690bfb2afd9f
eeeeuci <kioa1758@gmail.com>
2025-07-07 07:36:28 +0900

    CreatFKController_re

--- CreatFKController.py
+++ CreatFKController.py
@@ -1,23 +1,45 @@
-import maya.cmds as cmds 
+import maya.cmds as cmds
 
 jointNameList = cmds.ls(selection=True)
 
-for jointName in JointNameList:
-    print Jointname
+for jointName in jointNameList:
+    print(jointName)
     
-    creationController = cmds.circle(center=(0,0,0), normal1=(0,1,0), sweep=360, radius=1, degree=3,
-                                  useTolertance=False, tolerance=0, sections=8, constructionHistory=False, name=JointName+"_FK")
-    print createController[0]                              
+    createController = cmds.circle(
+        center=(0, 0, 0),
+        normal=(0, 1, 0),
+        sweep=360,
+        radius=1,
+        degree=1,
+        useTolerance=False,
+        tolerance=0,
+        sections=8,
+        constructionHistory=False,
+        name=jointName + "_FK"
+    )
+    print(createController[0])
     
-    controllerGroupName = cmds.group(createController[0], name=creatController[0]+"_POS")
-    print controllerGroupName
-    
-    positionConstraint = cmds.parentConstraint(jointName, controllerGroupName, Weight=1)
-    print positionConstraint[0]
-    
+    controllerGroupName = cmds.group(
+        createController[0],
+        name=createController[0] + "_POS"
+    )
+    print(controllerGroupName)
+    positionConstraint = cmds.parentConstraint(
+        jointName,
+        controllerGroupName,
+        weight=1
+    )
+    print(positionConstraint[0])
     cmds.delete(positionConstraint[0])
-    
-    cmds.parentConstraint(createController[0], jointName, maintainOffset=True, weight =1)
-    cmds.scaleConstraint(createController[0], jointName,maintainOffset=True, weight =1)
-    
-   
+    cmds.parentConstraint(
+        createController[0],
+        jointName,
+        maintainOffset=True,
+        weight=1
+    )
+    cmds.scaleConstraint(
+        createController[0],
+        jointName,
+        maintainOffset=True,
+        weight=1
+    )
--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Rigging: Rigging tools for Maya
 
 ### What is it?
-
-
+#### CreatFKController
+- reateFKControllerツールはSelectされているJointに自動的にCircleのControllerを付けてくれる基礎的なToolです。
+- Pythonを学び始め一番最初に作ったToolです。
+#### AutoControllerChaser
+- AutoControllerChaserツールは自分がカスタマイズしたControllerを選択したJointに自動的に付けてくれるToolです。特徴としては、Joint自体にTranslate、Radius、Scaleに数値が入ってもアニメーションを付けるには問題ないようにしました。
+- ダウンロードしたモデリングのJointに異常があったので、これらを問題ない環境でアニメーションを付けるようにこのようなToolを作りました。
+#### TrianglePolygonFinder
+- TrianglePolygonFinderツールはModelingの中にTriangleのPolygonを探して表示してくれるToolです。
+- ダウンロードしたモデリングにあまりにもTriangle polygonが多かったためこれらを探し、直すためこのようなツールを作りました。
 ### How to work?
-
+#### CreatFKController
+1. CircleControllerを付けたいJointをSelectします。
+2. 
 
 ### Demo
 
